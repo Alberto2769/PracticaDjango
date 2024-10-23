3c484f5c4486fc7a815e9af87cdbb6b465a8060c
Alberto2769 <jose1040.hum@gmail.com>
2024-10-23 14:08:45 -0600

    Plantilla Graficas

--- Api/login/login_view.py
+++ Api/login/login_view.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_views(request):
-    template_name = "auth-login-basic.html"
+    template_name = "login.html"
     
     return render(request,template_name)
